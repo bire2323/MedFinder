bc3a038092d595ee97696a3300a3ee1a26d5d8c6
birhanu derso tegegne <birhanuderso10@gmail.com>
2026-06-12 23:19:11 +0300

    Update API proxy target to production URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     // ←←← ADD THIS PROXY CONFIGURATION
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://medfinder-nqdq.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"), // optional but safe
