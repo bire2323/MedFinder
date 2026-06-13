5de59fc1d8ddd624d610d20f7fb57c387ba1459f
Birhanu Derso <birhanuderso10@gmail.com>
2026-06-13 12:02:47 +0300

    docker setup for backend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -16,6 +16,7 @@ return [
 
     'allowed_origins' => [
         'https://med-finder-five.vercel.app',
+        'https://medfinder-nqdq.onrender.com',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
